78d85fa336e8470129c1e762c271a62d9208592e
RAHUL <rahul@MTXs-MacBook-Pro-4.local>
2024-12-03 01:18:23 +0530

    added one more statement

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("this is java program");
         System.out.println("output for java program");
+        System.out.println(new Date());
     }
 }
